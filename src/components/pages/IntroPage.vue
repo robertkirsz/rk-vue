83ed90d15a7a217504e09178f0944c1c2a0d6782
Robert Kirsz <robert.kirsz@gmail.com>
2017-02-18 12:55:59 +0100

    Added new intro page

--- src/components/pages/IntroPage.vue
+++ src/components/pages/IntroPage.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="intro-page" @click="$router.push('/nav')">
-    <div class="content">
-      <h1>Robert Kirsz</h1>
-      <h2>Frontend developer</h2>
+  <div class="intro-page scanlines" @click="$router.push('/nav')">
+    <div class="title" data-parallax data-type="position" data-depth='0.5'>
+      <div class="name" data-parallax data-type="position" data-depth='0.4'></div>
     </div>
   </div>
 </template>
@@ -15,17 +14,29 @@
 
 <style lang="scss" scoped>
   @import "../../styles/variables";
+  @import "../../styles/scanlines";
 
   .intro-page {
     @extend %flex-center;
-    @extend %temp-font;
     @extend %transition;
     @extend %full-size;
-    background: $light-blue;
     user-select: none;
     cursor: pointer;
-    .content {
-      text-align: center;
+    background: url("/static/intro/background.jpg") no-repeat center center;
+    background-size: cover;
+    .title {
+      position: relative;
+      width: 90vw; height: 16vw;
+      background: url("/static/intro/title.png") no-repeat center center;
+      background-size: contain;
+      z-index: 1;
+    }
+    .name {
+      position: relative;
+      top: 9vw; left: 22vw;
+      width: 70vw; height: 21vw;
+      background: url("/static/intro/name.png") no-repeat center center;
+      background-size: contain;
     }
   }
 </style>
